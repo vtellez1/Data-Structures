4e63f6a289fafed45e4d1557543633184a040233
Vanessa <vanessatellez45@gmail.com>
2020-04-22 16:23:05 -0400

    day 1 of binary search tree complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+#from dll_queue import Queue
+#from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,26 +12,82 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # self.left and/or self.right need to be valid node to call insert on them
+        if value < self.value:
+            # check if self.left is a valid node
+            if self.left:
+                self.left.insert(value)
+            # the left side is empty
+            else:
+                # We've found a valid parking spot
+                self.left = BinarySearchTree(value)
+        #otherwise, value >= self.value
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # check is value is target, if so do not have to check children
+        if target == self.value:
+            return True
+        # otherwise, target is not self.value
+        else:
+            # if target is less than value, check left side
+            if target < self.value:
+                # Have to verify there is a left child
+                if self.left is not None:
+                    # Check left child with recursion
+                    return self.left.contains(target)
+                # If no left child, false
+                else:
+                    return False
+            # otherwise, target is greater than value, check right side
+            else:
+                # HAve to verify there is a right child
+                if self.right is not None:
+                    # If so, check if equals our target with recursion
+                    return self.right.contains(target)
+                else:
+                    return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # child on most right should be the greatest number
+        if self.right is None:
+            # if no self.right then the parent (self.value) should be greater than any left children
+            return self.value
+        # if there is a right child, recursively run and keep checking until we cannot
+        else:
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
+
     def for_each(self, cb):
-        pass
+        # Fist need to run cb on the parent
+        cb(self.value)
+        # If there is a left child, we need to run cb on it
+        if self.left:
+            self.left.for_each(cb)
+        # If not left child, we do nothing
+        else:
+            None
+        # if there is a right child, we need to run cb on it
+        if self.right:
+            self.right.for_each(cb)
+        # If not right child, we do nothing
+        else:
+            None
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
         pass
 
